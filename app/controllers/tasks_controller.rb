c63abfc3c25b755d39a1e78dec5895257d615fad
Kirill Zhuravlov <tjai89@mail.ru>
2016-04-16 16:49:19 +0300

    443 from 419

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+  def create
+    @task = Task.new(
+      params[:task].permit(:project_id, :title, :size))
+    redirect_to @task.project
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317153621) do
+ActiveRecord::Schema.define(version: 20160415190637) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20160317153621) do
     t.string   "title"
     t.integer  "size"
     t.datetime "completed_at"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "project_order"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
